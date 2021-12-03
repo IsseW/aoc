84a743c7bf95651cfd482d4c45a7da0e6cac34bc
IsseW <isidornie@gmail.com>
2021-12-03 12:28:51 +0100

    day 3

--- src/y2021/d3.rs
+++ src/y2021/d3.rs
@@ -1,9 +1,87 @@
 use crate::helpers;
 
 pub fn solution_1(input: &str) -> String {
-	"Not yet implemented".into()
+    let mut bits = [0; 12];
+    let mut num_lines = 0;
+    input.lines().for_each(|line| {
+        num_lines += 1;
+        for (i, char) in line.chars().enumerate() {
+            match char {
+                '1' => bits[i] += 1,
+                _ => {}
+            }
+        }
+    });
+    let mut a = 0;
+    let mut b = 0;
+    for i in 0..12 {
+        if bits[i] > num_lines / 2 {
+            a |= 1 << (11 - i);
+        } else {
+            b |= 1 << (11 - i);
+        }
+    }
+    (a * b).to_string()
 }
 
 pub fn solution_2(input: &str) -> String {
-	"Not yet implemented".into()
+    const LEN: usize = 12;
+    let mut numbers: Vec<[bool; LEN]> = Vec::new();
+    let mut num_lines = 0;
+    input.lines().for_each(|line| {
+        num_lines += 1;
+        let mut local_bits = [false; LEN];
+        for (i, char) in line.chars().enumerate() {
+            match char {
+                '1' => {
+                    local_bits[i] = true;
+                }
+                _ => {}
+            }
+        }
+        numbers.push(local_bits);
+    });
+    let mut numbers2 = numbers.clone();
+    for i in 0..LEN {
+        let mut count = 0;
+        for n in &numbers {
+            if n[i] {
+                count += 1;
+            }
+        }
+        let all = numbers.len();
+        if numbers.len() > 1 {
+            numbers.drain_filter(|number| {
+                if count >= all - count {
+                    !number[i]
+                } else {
+                    number[i]
+                }
+            });
+        }
+        let mut count = 0;
+        for n in &numbers2 {
+            if n[i] {
+                count += 1;
+            }
+        }
+        let all = numbers2.len();
+        if numbers2.len() > 1 {
+            numbers2.drain_filter(|number| {
+                if count >= all - count {
+                    number[i]
+                } else {
+                    !number[i]
+                }
+            });
+        }
+        //println!("{:#?}", numbers);
+    }
+    let mut a = 0;
+    let mut b = 0;
+    for i in 0..LEN {
+        a |= if numbers[0][i] { 1 } else { 0 } << (LEN - 1 - i);
+        b |= if numbers2[0][i] { 1 } else { 0 } << (LEN - 1 - i);
+    }
+    (a * b).to_string()
 }
