0ff40160c0c582128c3b914b42f59d7922355dfd
Isse <isidornie@gmail.com>
2022-12-02 16:11:38 +0100

    fmt

--- src/y2022/d2.rs
+++ src/y2022/d2.rs
@@ -1,7 +1,7 @@
 use crate::helpers;
 
 fn parse<'a>(input: &'a str) -> impl Iterator<Item = (u8, u8)> + 'a {
-	input.lines().filter_map(|line| {
+    input.lines().filter_map(|line| {
 		let [e, p] = *line.split_whitespace().filter_map(|s| (s.len() == 1).then(|| s.chars().next()).flatten()).collect::<Vec<_>>() else {
 			return None;
 		};
@@ -10,25 +10,31 @@ fn parse<'a>(input: &'a str) -> impl Iterator<Item = (u8, u8)> + 'a {
 }
 
 pub fn solution_1(input: &str) -> String {
-	parse(input).map(|(e, p)| {
-		let r = (4 + p - e) % 3;
+    parse(input)
+        .map(|(e, p)| {
+            let r = (4 + p - e) % 3;
 
-		let win_points = r * 3;
+            let win_points = r * 3;
 
-		let play_points = p + 1;
+            let play_points = p + 1;
 
-		(win_points + play_points) as u32
-	}).sum::<u32>().to_string()
+            (win_points + play_points) as u32
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 pub fn solution_2(input: &str) -> String {
-	parse(input).map(|(e, r)| {
-		let p = (e + r + 2) % 3;
+    parse(input)
+        .map(|(e, r)| {
+            let p = (e + r + 2) % 3;
 
-		let play_points = p + 1;
+            let play_points = p + 1;
 
-		let win_points = r * 3;
+            let win_points = r * 3;
 
-		(win_points + play_points) as u32
-	}).sum::<u32>().to_string()
+            (win_points + play_points) as u32
+        })
+        .sum::<u32>()
+        .to_string()
 }
