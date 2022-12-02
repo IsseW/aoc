7087bf2c8b574abcf1cd492ff7d6864b5a0a900d
Isse <isidornie@gmail.com>
2022-12-02 16:11:01 +0100

    day 2

--- src/y2022/d2.rs
+++ src/y2022/d2.rs
@@ -1,9 +1,34 @@
 use crate::helpers;
 
+fn parse<'a>(input: &'a str) -> impl Iterator<Item = (u8, u8)> + 'a {
+	input.lines().filter_map(|line| {
+		let [e, p] = *line.split_whitespace().filter_map(|s| (s.len() == 1).then(|| s.chars().next()).flatten()).collect::<Vec<_>>() else {
+			return None;
+		};
+		Some((e as u8 - b'A', p as u8 - b'X'))
+	})
+}
+
 pub fn solution_1(input: &str) -> String {
-	"Not yet implemented".into()
+	parse(input).map(|(e, p)| {
+		let r = (4 + p - e) % 3;
+
+		let win_points = r * 3;
+
+		let play_points = p + 1;
+
+		(win_points + play_points) as u32
+	}).sum::<u32>().to_string()
 }
 
 pub fn solution_2(input: &str) -> String {
-	"Not yet implemented".into()
+	parse(input).map(|(e, r)| {
+		let p = (e + r + 2) % 3;
+
+		let play_points = p + 1;
+
+		let win_points = r * 3;
+
+		(win_points + play_points) as u32
+	}).sum::<u32>().to_string()
 }
