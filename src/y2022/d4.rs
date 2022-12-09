7e603c9840432880c883c62f366769a2d73329b0
Isse <isidornie@gmail.com>
2022-12-09 07:40:34 +0100

    rev

--- src/y2022/d4.rs
+++ src/y2022/d4.rs
@@ -1,7 +1,7 @@
 fn parse<'a>(input: &'a str) -> impl Iterator<Item = ((u32, u32), (u32, u32))> + 'a {
-	input.lines().filter_map(|ranges| {
-		let res: (u32, u32, u32, u32) = strp::try_scan!(ranges => "{}-{},{}-{}").ok()?;
-		Some(((res.0, res.1), (res.2, res.3)))
+	input.lines().map(|ranges| {
+		let res: (u32, u32, u32, u32) = strp::scan!(ranges => "{}-{},{}-{}");
+		((res.0, res.1), (res.2, res.3))
 	})
 }
 
