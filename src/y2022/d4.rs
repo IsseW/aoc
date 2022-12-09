904021637a4cd01a03d6d0c5d7054b11d51ef2dd
Isse <isidornie@gmail.com>
2022-12-09 07:21:55 +0100

    day 9

--- src/y2022/d4.rs
+++ src/y2022/d4.rs
@@ -2,14 +2,13 @@ use std::ops::RangeInclusive;
 
 use crate::helpers;
 
-fn parse<'a>(input: &'a str) -> impl Iterator<Item = (RangeInclusive<u32>, RangeInclusive<u32>)> + 'a {
+fn parse<'a>(input: &'a str) -> impl Iterator<Item = ((u32, u32), (u32, u32))> + 'a {
 	input.lines().filter_map(|ranges| {
 		let (r1, r2) = ranges.split_once(',')?;
 		let parse_range = |range: &str| {
 			let (a, b) = range.split_once("-")?;
-			Some(a.parse::<u32>().ok()?..=b.parse::<u32>().ok()?)
+			Some((a.parse::<u32>().ok()?, b.parse::<u32>().ok()?))
 		};
-
 		let r1 = parse_range(r1)?;
 		let r2 = parse_range(r2)?;
 		Some((r1, r2))
@@ -17,13 +16,9 @@ fn parse<'a>(input: &'a str) -> impl Iterator<Item = (RangeInclusive<u32>, Range
 }
 
 pub fn solution_1(input: &str) -> String {
-	parse(input).filter(|(a, b)| {
-		(a.contains(&b.start()) && a.contains(&b.end())) || (b.contains(&a.start()) && b.contains(&a.end()))
-	}).count().to_string()
+	parse(input).filter(|(a, b)| (a.0 <= b.0 && a.1 >= b.1) || (b.0 <= a.0 && b.1 >= a.1)).count().to_string()
 }
 
 pub fn solution_2(input: &str) -> String {
-	parse(input).filter(|(a, b)| {
-		a.contains(&b.start()) || a.contains(&b.end()) || b.contains(&a.start())
-	}).count().to_string()
+	parse(input).filter(|(a, b)| (a.0 >= b.0 && a.0 <= b.1) || (a.1 >= b.0 && a.1 <= b.1) || (b.0 >= a.0 && b.0 <= a.1)).count().to_string()
 }
--- src/y2022/d9.rs
+++ src/y2022/d9.rs
@@ -1,9 +1,101 @@
+use hashbrown::HashSet;
+use vek::Vec2;
+
 use crate::helpers;
 
 pub fn solution_1(input: &str) -> String {
-	"Not yet implemented".into()
+	let mut set = HashSet::new();
+	let mut head_pos = Vec2::<i32>::zero();
+	let mut tail_pos = Vec2::<i32>::zero();
+	set.insert(tail_pos);
+	input.lines().for_each(|line| {
+		let (dir, amount) = line.split_once(' ').unwrap();
+		let dir = match dir {
+			"R" => Vec2::new(1, 0),
+			"L" => Vec2::new(-1, 0),
+			"U" => Vec2::new(0, -1),
+			"D" => Vec2::new(0, 1),
+			_ => panic!(),
+		};
+		let amount = amount.parse::<i32>().unwrap();
+		for _ in 0..amount {
+			head_pos += dir;
+			let diff = (tail_pos - head_pos).as_::<f32>().normalized().map(|e| e.round() as i32);
+			tail_pos = head_pos + diff;
+			set.insert(tail_pos);
+		}
+	});
+
+	set.len().to_string()
 }
 
 pub fn solution_2(input: &str) -> String {
-	"Not yet implemented".into()
+	let mut set = HashSet::new();
+	let mut positions = [Vec2::<i32>::zero(); 10];
+	let mut min = Vec2::<i32>::zero();
+	let mut max = Vec2::<i32>::zero();
+	set.insert(positions[8]);
+	input.lines().for_each(|line| {
+		let (dir, amount) = line.split_once(' ').unwrap();
+		let dir = match dir {
+			"R" => Vec2::new(1, 0),
+			"L" => Vec2::new(-1, 0),
+			"U" => Vec2::new(0, -1),
+			"D" => Vec2::new(0, 1),
+			_ => panic!(),
+		};
+		let amount = amount.parse::<i32>().unwrap();
+		for _ in 0..amount {
+			positions[0] += dir;
+			let mut head_pos = positions[0];
+			for tail_pos in &mut positions[1..] {
+				let diff = *tail_pos - head_pos;
+				if diff.magnitude_squared() == 0 {
+					continue;
+				}
+				let diff = diff.as_::<f32>().normalized().map(|e| e.round() as i32);
+				*tail_pos = head_pos + diff;
+				head_pos = *tail_pos;
+			}
+			set.insert(*positions.last().unwrap());
+			min = min.map2(positions.iter().copied().reduce(|a, b| a.map2(b, |a, b| a.min(b))).unwrap(), |a, b| a.min(b));
+			max = max.map2(positions.iter().copied().reduce(|a, b| a.map2(b, |a, b| a.max(b))).unwrap(), |a, b| a.max(b));
+		}
+
+		/* 
+		println!("\n== {line} ==\n");
+		for y in min.y..=max.y {
+			for x in min.x..=max.x {
+				let pos = Vec2::new(x, y);
+				if let Some((i, _)) = positions.iter().enumerate().find(|(_, p)| **p == pos) {
+					if i == 0 {
+						print!("H");
+					} else {
+						print!("{}", i);
+					}
+				} else if pos == Vec2::zero() {
+					print!("s");
+				} else {
+					print!(".");
+				}
+			}
+			println!()
+		}
+		*/
+	});
+	/*
+	println!("\n\n\n");
+
+	for y in min.y..=max.y {
+		for x in min.x..=max.x {
+			if set.contains(&Vec2::new(x, y)) {
+				print!("#")
+			} else {
+				print!(".");
+			}
+		}
+		println!()
+	}
+	*/
+	set.len().to_string()
 }
