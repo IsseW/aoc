0c080116e324e787b03ca0aa419afa3da7541f37
Isse <isidornie@gmail.com>
2023-12-04 20:57:54 +0100

    ok

--- src/y2023/d4.rs
+++ src/y2023/d4.rs
@@ -1,12 +1,6 @@
 use bitvec::array::BitArray;
 
-#[derive(Clone, Copy)]
-struct Card {
-    count: u32,
-    wins: u32,
-}
-
-fn parse_input(input: &str) -> impl Iterator<Item = Card> + '_ {
+fn parse_input(input: &str) -> impl Iterator<Item = u32> + '_ {
     input.lines().map(|line| {
         let line = &line["Card ".len()..];
         let (id, numbers) = line.split_once(':').unwrap();
@@ -28,43 +22,30 @@ fn parse_input(input: &str) -> impl Iterator<Item = Card> + '_ {
             wins += winning[i] as u32;
         }
 
-        Card { count: 1, wins }
+        wins
     })
 }
 
 pub fn solution_1(input: &str) -> String {
     parse_input(input)
-        .map(|card| {
-            if card.wins == 0 {
-                0
-            } else {
-                1 << (card.wins - 1)
-            }
-        })
+        .map(|wins| if wins == 0 { 0 } else { 1 << (wins - 1) })
         .sum::<u64>()
         .to_string()
 }
 
 pub fn solution_2(input: &str) -> String {
-    let mut cards: Vec<_> = parse_input(input).collect();
-    for i in 0..cards.len() {
-        let card = cards[i];
-        for i in i + 1..(i + 1 + card.wins as usize).min(cards.len()) {
-            cards[i].count += card.count;
+    let mut counts = [1; 11];
+    let mut i = 0;
+    let mut total: u64 = 0;
+    for wins in parse_input(input) {
+        let j = i % counts.len();
+        let c = counts[j];
+        total += c;
+        counts[j] = 1;
+        i += 1;
+        for j in i..i + wins as usize {
+            counts[j % counts.len()] += c;
         }
     }
-    cards
-        .into_iter()
-        .map(|card| card.count)
-        .sum::<u32>()
-        .to_string()
-    // let mut counts = vec![1; cards.len()];
-    // for (i, card) in cards.iter().enumerate() {
-    //     let wins = (card.ours & card.winning).count_ones();
-    //     let count = counts[i];
-    //     for v in &mut counts[i + 1..(i + 1 + wins).min(cards.len())] {
-    //         *v += count;
-    //     }
-    // }
-    // counts.iter().sum::<u128>().to_string()
+    total.to_string()
 }
